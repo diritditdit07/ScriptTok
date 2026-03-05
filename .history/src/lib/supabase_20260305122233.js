2f57cb56e563b6b319178e209660883216f0a3f8
diritditdit07 <aditpermanaputra36@gmail.com>
2026-03-05 12:24:17 +0700

    update supabase config

---
+++ .history/src/lib/supabase_20260305122233.js
@@ -0,0 +1,6 @@
+import { createClient } from '@supabase/supabase-js'
+
+const supabaseUrl = "https://aubvjxglmgrqqcsnwodr.supabase.co"
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InBnbWJubmFpa29samJpc3J2dHRiIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI2ODAxMzAsImV4cCI6MjA4ODI1NjEzMH0.UrLiMhGWHLKZ7_Jv1UFDuAaFTbcVf1QPMaxq3syrrNE"
+
+export const supabase = createClient(supabaseUrl, supabaseKey)
